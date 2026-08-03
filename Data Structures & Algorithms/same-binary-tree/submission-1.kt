3e9c7c27feedc8794b627fd7c40d46ea697e0e68
Kelly Ge <36249982+Shiney-Kelly@users.noreply.github.com>
2026-08-03 14:16:00 -0400

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.kt
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        // BFS - FIFO
+        if(p == null && q != null) return false
+        if(q == null && p != null) return false
+
+        val queue = ArrayDeque<Pair<TreeNode?, TreeNode?>>()
+
+        queue.add(Pair(p,q))
+
+        while(queue.isNotEmpty()){
+            val (nodeP, nodeQ) = queue.removeFirst()
+            if(nodeP == null && nodeQ == null) continue
+            if(nodeP == null || nodeQ == null) return false
+            if(nodeP.`val` != nodeQ.`val`) return false
+
+            queue.add(Pair(nodeP.left, nodeQ.left))
+            queue.add(Pair(nodeP.right, nodeQ.right))
+        }
+
+        return true
+        
+
+    }
+}
